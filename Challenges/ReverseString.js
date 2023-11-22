76cc9096574265ff064f5d5d19e185ce899bf375
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2023-11-22 16:30:28 -0700

    Revese String

---
+++ Challenges/ReverseString.js
@@ -0,0 +1,7 @@
+let intialString = "This is my boom stick"
+console.log(intialString)
+intialString = intialString.split("")
+intialString = intialString.reverse()
+intialString = intialString.join("")
+console.log(intialString)
+
